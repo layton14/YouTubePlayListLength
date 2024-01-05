511614713e2e8b838544d9edc46783c3df957bfd
layton14 <layton14@gmail.com>
2024-01-04 20:50:49 -0800

    updated code + comments

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+
+//Sets up the nodes that should evaluated, calls the other functions
 function getPlaylistLength() {
     const ytTimeNodes = document.querySelectorAll(".style-scope ytd-thumbnail-overlay-time-status-renderer");
     let sum = 0;
     
-    ytTimeNodes.forEach((ele) => sum += parseTime(ele.innerText));
+    ytTimeNodes.forEach((element) => sum += parseTime(element.innerText));
 
-    console.log("Current Playlist Total Time: ", parseDisplayTime(sum));
-
+    const playlistLength = parseDisplayTime(sum)
+    
+    return playlistLength;
 }
 
+//Takes in a (int) time in seconds and returns the time in "hh:mm:ss" format
 function parseDisplayTime(totalSeconds) {
-    let hours = Math.floor(totalSeconds / 3600);
-    let minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
-    let seconds = totalSeconds - (hours * 3600) - (minutes * 60);
-    let timeString = hours.toString().padStart(2, '0') + ':' + 
-          minutes.toString().padStart(2, '0') + ':' + 
-          seconds.toString().padStart(2, '0');
+    const secondsPerHour = 3600;
+    const minutesPerHour = 60;
+    
+    const hours = Math.floor(totalSeconds / secondsPerHour);
+    const minutes = Math.floor((totalSeconds - (hours * secondsPerHour)) / minutesPerHour);
+    const seconds = totalSeconds - (hours * secondsPerHour) - (minutes * minutesPerHour);
+    const timeString =
+            hours.toString().padStart(2, '0') + 'h:' + 
+            minutes.toString().padStart(2, '0') + 'm:' + 
+            seconds.toString().padStart(2, '0') + 's';
 
     return timeString;
 }
 
+//Takes in a string "time" (in "hh:mm") format and returns the total time in (int) seconds for further calculation 
 function parseTime(inText) {
     const minutes = parseInt(inText.substring(0, inText.indexOf(":")));
     const seconds = parseInt(inText.substring(inText.indexOf(":") + 1, inText.length));
@@ -26,4 +35,4 @@ function parseTime(inText) {
     return ((minutes * 60) + seconds);
 }
 
-getPlaylistLength();
+console.log("Current Playlist Total Time: ", getPlaylistLength());
